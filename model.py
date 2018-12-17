14b1d9f007656859c4ef0bad6882292fb9c71168
landOfTigers <hanna.e.becker@gmail.com>
2018-12-17 13:05:39 +0000

    add and train simple model

--- model.py
+++ model.py
@@ -17,3 +17,15 @@ with open('data/driving_log.csv') as csvfile:
 
 X_train = np.array(images) # shape: (8036, 160, 320, 3)
 y_train = np.array(measurements) # shape: (8036,)
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.save('model.h5')
