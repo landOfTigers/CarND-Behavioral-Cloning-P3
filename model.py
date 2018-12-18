eb159e3d818f0960114f4ba4288be691ceb53294
landOfTigers <hanna.e.becker@gmail.com>
2018-12-18 21:12:29 +0000

    use resnet model, use left and right camera images do not commit model.h5, as it is too large for github

--- model.py
+++ model.py
@@ -2,14 +2,25 @@ from readTrainingData import readTrainingData
 
 X_train, y_train = readTrainingData()
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.applications.resnet50 import ResNet50
+from keras.layers import Input, Lambda, Dense, GlobalAveragePooling2D
+from keras.models import Model
+from keras import backend as K
+import tensorflow as tf
 
-model = Sequential()
-model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(160,320,3)))
-model.add(Flatten())
-model.add(Dense(1))
+rawInput = Input(shape=(160,320,3))
+normalizedInput = Lambda(lambda x: x/255.0-0.5)(rawInput)
+resizedInput = Lambda(lambda image: K.tf.image.resize_images(image, (197, 197)))(normalizedInput)
+
+resNet = ResNet50(weights='imagenet', include_top=False, input_shape=(197,197,3))
+inp = resNet(resizedInput)
+out = GlobalAveragePooling2D()(inp)
+# new_layer = Dense(512, activation='relu')(out)
+predictions = Dense(1)(out)
+
+model = Model(inputs=rawInput, outputs=predictions)
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+model.summary()
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 model.save('model.h5')
--- readTrainingData.py
+++ readTrainingData.py
@@ -5,24 +5,33 @@ import numpy as np
 
 def readTrainingData():
     images = []
-    measurements = []
+    steeringAngles = []
     with open('data/driving_log.csv') as csvfile:
         print('Reading in training data...')
         reader = iter(csv.reader(csvfile))
         next(reader) # skip first line
         printInc = 1000
         for i, line in enumerate(reader):
+            # print progress
             if (i%printInc == 0) and (i != 0):
                 print('Finished lines ' + str(i-printInc) + '-'+ str(i))
-            sourcePath = line[0]
-            fileName = sourcePath.split('/')[-1]
-            currentPath = 'data/IMG/' + fileName
-            images.append(cv2.imread(currentPath))
-            measurements.append(float(line[3]))
+                
+            # add images
+            imageCenter = cv2.imread('data/IMG/' + line[0].split('/')[-1])
+            imageLeft = cv2.imread('data/IMG/' + line[1].split('/')[-1])
+            imageRight = cv2.imread('data/IMG/' + line[2].split('/')[-1])
+            images.extend([imageCenter, imageLeft, imageRight])
             
+            # add steering measurements
+            correction = 0.02
+            steeringCenter = float(line[3])
+            steeringLeft = steeringCenter + correction
+            steeringRight = steeringCenter - correction
+            steeringAngles.extend([steeringCenter, steeringLeft, steeringRight])
+           
         print('Finished reading in training data.')
         
     X_train = np.array(images) # shape: (8036, 160, 320, 3)
-    y_train = np.array(measurements) # shape: (8036,)
+    y_train = np.array(steeringAngles) # shape: (8036,)
     
     return X_train, y_train
