09230bf83d19d3d428352ad52d37ef3556c0df02
landOfTigers <hanna.e.becker@gmail.com>
2018-12-21 11:21:33 +0000

    fix download link and improve bash script

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-wget https://drive.google.com/file/d/16D-Ob5qTmuVBwc7nGmeE4jallRaqcKkw/view?usp=sharing -O data/backwards.zip
+wget https://doc-0k-5k-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/leop05q9t3406jh53jvkjsatse027o71/1545386400000/05918670026020017381/*/16D-Ob5qTmuVBwc7nGmeE4jallRaqcKkw?e=download -O data/backwards.zip
+
+# unzip
+cd data
+unzip backwards.zip -d data
+rm -f backwards.zip
+
+# move files
+mv backwards/driving_log.csv driving_log_backwards.csv
+find IMG/ -type f | wc -l # print number of images before
+mv backwards/IMG/* IMG/
+find IMG/ -type f | wc -l # print number of images after
+
+rm -rf backwards
+
+cd ..
